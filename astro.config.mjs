9df52edd9b9ceaefa46baca90899661d758e636c
David Galiata | neoslash.net <david.galiata@aquia.io>
2024-08-01 18:06:30 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   site: "https://galiata.blog"
 });
